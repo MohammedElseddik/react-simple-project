13d38affe5159304fe3b87f7c4750b6e5fff1da0
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-05-25 18:50:54 +0100

    Create user list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddUser from './components/users/AddUser';
+import UserList from './components/users/UserList';
 
 function App() {
   return (
     <div className="App">
       <AddUser />
+      <UserList />
     </div>
   );
 }
