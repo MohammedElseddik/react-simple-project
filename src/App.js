0462f045473c65e741e7006f2f386cddd116f001
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-05-26 12:39:44 +0100

    Test the application with hard coded data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import AddUser from './components/users/AddUser';
 import UserList from './components/users/UserList';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   return (
     <div className="App">
       <AddUser />
-      <UserList users={[]} />
+      <UserList
+        users={[
+          { id: uuidv4(), name: 'Sami', age: '34' },
+          { id: uuidv4(), name: 'Josh', age: '33' },
+          { id: uuidv4(), name: 'Drake', age: '32' },
+        ]}
+      />
     </div>
   );
 }
