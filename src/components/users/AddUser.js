a56b3079f3e256bf643be2aff6a3e505339f075c
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-05-26 12:22:12 +0100

    Add validation logic

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../button/buttons';
 
 const AddUser = (props) => {
+  const [nameInput, setNameInput] = useState('');
+  const [ageInput, setageInput] = useState('');
+
+  const nameInputHandler = (event) => {
+    setNameInput(event.target.value);
+  };
+
+  const ageInputHandler = (event) => {
+    setageInput(event.target.value);
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(parseInt(ageInput));
+    if (nameInput.trim().length === 0 || ageInput.trim().length === 0) return;
+
+    if (parseInt(ageInput) < 1) return;
+
+    console.log(nameInput, ageInput);
+    setNameInput('');
+    setageInput('');
   };
+
   return (
     <form onSubmit={addUserHandler}>
       <label htmlFor="user-name">User Name</label>
-      <input id="user-name" type="text" required />
+      <input id="user-name" type="text" required onChange={nameInputHandler} />
       <label htmlFor="age">User Name</label>
-      <input id="age" type="text" required />
+      <input id="age" type="number" required onChange={ageInputHandler} />
       <Button type="submit">Add User</Button>
     </form>
   );
