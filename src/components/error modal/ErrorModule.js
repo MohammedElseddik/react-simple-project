525623bd78976487d14110f860f00214871dd879
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-05-31 13:38:58 +0100

    Manage the state of the error message

--- src/components/error modal/ErrorModule.js
+++ src/components/error modal/ErrorModule.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const ErrorModule = (props) => {
+  console.log(props);
   return (
     <div>
       <header>
--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import Button from '../button/buttons';
+import ErrorModule from '../error modal/ErrorModule';
 
 const AddUser = (props) => {
   const [nameInput, setNameInput] = useState('');
   const [ageInput, setageInput] = useState('');
+  const [error, setError] = useState(null);
 
   const nameInputHandler = (event) => {
     setNameInput(event.target.value);
@@ -20,34 +22,43 @@ const AddUser = (props) => {
       nameInput.trim().length === 0 ||
       ageInput.trim().length === 0 ||
       parseInt(ageInput) < 1
-    )
+    ) {
+      setError({ message: 'you have entered an invalid email or age' });
       return;
+    }
     console.log('hello', nameInput, ageInput);
     props.onAddUser(nameInput, ageInput);
     setNameInput('');
     setageInput('');
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="user-name">User Name</label>
-      <input
-        id="user-name"
-        type="text"
-        value={nameInput}
-        required
-        onChange={nameInputHandler}
-      />
-      <label htmlFor="age">User Name</label>
-      <input
-        id="age"
-        type="number"
-        value={ageInput}
-        required
-        onChange={ageInputHandler}
-      />
-      <Button type="submit">Add User</Button>
-    </form>
+    <>
+      {error && <ErrorModule errorMessage={error.message} />}
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="user-name">User Name</label>
+        <input
+          id="user-name"
+          type="text"
+          value={nameInput}
+          required
+          onChange={nameInputHandler}
+        />
+        <label htmlFor="age">User Name</label>
+        <input
+          id="age"
+          type="number"
+          value={ageInput}
+          required
+          onChange={ageInputHandler}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </>
   );
 };
 
